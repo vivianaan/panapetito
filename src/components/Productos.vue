5595ef4b699f5306af605e8e260a9e4716b5f9f5
Viviana Manzano <vivianitandrea18@gmail.com>
2024-06-15 14:03:48 -0500

    Estilos css al componente de Productos

--- src/components/Productos.vue
+++ src/components/Productos.vue
@@ -2,12 +2,12 @@
     <div class="contenedor">
     
     <Titulo :titulo="props.categoria" />
-    <div class="producto">
+    <div class="productos">
         <div class="producto" v-for="producto in productos" :key="producto.id" >
             <img class="foto" :src="`/imagenes/${producto.imagen}`" alt="Producto">
             <div class="informacion">
                 <h3 class="titulo">
-                    {{producto.nombre}}
+                    {{producto.producto}}
                 </h3>
                 <p class="descripcion">
                     {{producto.descripcion}}
@@ -35,6 +35,75 @@ const productos = ref([])
 
 watchEffect(() => {
     productos.value = listaProductos.filter((producto) => producto.categoria === props.categoria)
-
 })
 </script>
+
+<style scoped>
+.contenedor{
+    padding: 3rem;
+    margin-bottom: 2rem;
+}
+.productos {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 2em;
+}
+.producto {
+    display: flex;
+    align-items: center;
+}
+.informacion {
+    padding: 15px;
+}
+.titulo{
+    font-size: 1.25rem;
+}
+.descripcion{
+    color: gray;
+    word-wrap: break-word;
+}
+.foto {
+    max-width: calc(100% / 4);
+    border-radius: 20px;
+    box-shadow: 0px 2px 4px rgb(76, 76, 76)
+}
+.precio{
+    color: #f9a72b;
+    font-weight: bold;
+    font-size: 1.25rem;
+}
+
+@media (max-width: 1024px) and (min-width: 768px) {
+    .productos {
+        grid-template-columns: repeat(2, 1fr);
+    }
+}
+
+@media (max-width: 768px) {
+    .productos {
+        grid-template-columns: repeat(1, 1fr);
+    }
+}
+
+
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
